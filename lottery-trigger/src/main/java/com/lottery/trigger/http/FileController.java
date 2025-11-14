3730548d88e13430b84df74ccdd06c36bbe4f124
userwanyong <2026804718@qq.com>
2025-11-14 21:56:48 +0800

    feat: upload file alioss->minio

--- lottery-trigger/src/main/java/com/lottery/trigger/http/FileController.java
+++ lottery-trigger/src/main/java/com/lottery/trigger/http/FileController.java
@@ -3,6 +3,8 @@ package com.lottery.trigger.http;
 import com.lottery.types.enums.ResponseCode;
 import com.lottery.types.model.BaseResponse;
 import com.lottery.types.util.AliOSSUtils;
+import com.lottery.types.util.MinIOUtils;
+import io.minio.errors.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author 永
@@ -24,6 +28,8 @@ public class FileController {
 
     @Autowired
     private AliOSSUtils aliOSSUtils;
+    @Autowired
+    private MinIOUtils minIOUtils;
 
     /**
      * 文件上传
@@ -32,10 +38,10 @@ public class FileController {
      * @throws IOException io
      */
     @PostMapping("/upload")
-    public BaseResponse<String> upload(MultipartFile file) throws IOException {
+    public BaseResponse<String> upload(MultipartFile file) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         log.info("文件上传，文件名:{}",file.getOriginalFilename());
         //调用阿里云OSS工具类进行上传
-        String url = aliOSSUtils.upload(file);
+        String url = minIOUtils.upload(file);
         log.info("文件上传成功，文件访问地址为：{}",url);
         return new BaseResponse<>(ResponseCode.SUCCESS.getCode(), url);
     }
---
+++ lottery-types/src/main/java/com/lottery/types/util/MinIOUtils.java
@@ -0,0 +1,129 @@
+package com.lottery.types.util;
+
+
+import io.minio.*;
+import io.minio.errors.*;
+import io.minio.http.Method;
+import io.minio.messages.Bucket;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * @author 永
+ */
+@Component
+public class MinIOUtils {
+
+    @Value("${minio.endpoint}")
+    private String endpoint;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.accessSecret}")
+    private String accessSecret;
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+    private MinioClient minioClient;
+
+    @PostConstruct
+    public void init() throws ServerException, InsufficientDataException, ErrorResponseException,
+            NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException,
+            XmlParserException, InternalException, IOException {
+        minioClient = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, accessSecret)
+                .build();
+
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        if (!found) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        }
+    }
+
+    /**
+     * 上传并返回url
+     */
+    public String upload(MultipartFile file) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        //获取上传的文件的输入流
+        InputStream inputStream = file.getInputStream();
+        //避免文件覆盖
+        String originalFileName = file.getOriginalFilename();
+        String fileName = UUID.randomUUID() + originalFileName.substring(originalFileName.lastIndexOf("."));
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .stream(inputStream, inputStream.available(), -1)
+                        .contentType(file.getContentType())
+                        .build());
+        GetPresignedObjectUrlArgs args = GetPresignedObjectUrlArgs.builder()
+                .method(Method.GET)
+                .bucket(bucketName).object(fileName).build();
+
+        return minioClient.getPresignedObjectUrl(args);
+    }
+
+    /**
+     * 上传文件
+     */
+    public void uploadFile(InputStream inputStream, String bucket, String objectName) throws Exception {
+        minioClient.putObject(PutObjectArgs.builder().bucket(bucket).object(objectName)
+                .stream(inputStream, -1, 5242889L).build());
+    }
+
+    /**
+     * 列出所有桶
+     */
+    public List<String> getAllBucket() throws Exception {
+        List<Bucket> buckets = minioClient.listBuckets();
+        return buckets.stream().map(Bucket::name).collect(Collectors.toList());
+    }
+
+    /**
+     * 下载文件
+     */
+    public InputStream downLoad(String bucket, String objectName) throws Exception {
+        return minioClient.getObject(
+                GetObjectArgs.builder().bucket(bucket).object(objectName).build()
+        );
+    }
+
+    /**
+     * 删除桶
+     */
+    public void deleteBucket(String bucket) throws Exception {
+        minioClient.removeBucket(
+                RemoveBucketArgs.builder().bucket(bucket).build()
+        );
+    }
+
+    /**
+     * 删除文件
+     */
+    public void deleteObject(String bucket, String objectName) throws Exception {
+        minioClient.removeObject(
+                RemoveObjectArgs.builder().bucket(bucket).object(objectName).build()
+        );
+    }
+
+    /**
+     * 获取文件url
+     */
+    public String getPreviewFileUrl(String bucketName, String objectName) throws Exception {
+        GetPresignedObjectUrlArgs args = GetPresignedObjectUrlArgs.builder()
+                .method(Method.GET)
+                .bucket(bucketName).object(objectName).build();
+        return minioClient.getPresignedObjectUrl(args);
+    }
+
+}
